9eb91e4e43da9f36eb90b5a1683776f825ce6a62
abdul-moize <abdul.moize@arbisoft.com>
2021-09-09 11:03:45 +0500

    add contants.py, add utils.py, refactor variable names and code

--- tutorial/spiders/pakistan_stores_spider.py
+++ tutorial/spiders/pakistan_stores_spider.py
@@ -2,10 +2,12 @@
 This module contains a spider of pakistanistores.com site
 that crawls and extracts data
 """
-import json
 from functools import partial
 
 import scrapy
+
+from ..constants import TEXT_PROP
+from ..utils import write_to_file
 
 
 class PakistanStoresSpider(scrapy.Spider):
@@ -32,17 +34,19 @@ class PakistanStoresSpider(scrapy.Spider):
                             Or
                             None if everything processed
         """
-        last_page = response.css("a.page-link.navigate::attr(data-href)")[-1].get()
+        last_page = response.css("a.page-link.navigate")[-1].attrib["data-href"]
         last_page_number = int(last_page[last_page.find("=") + 1 :])
         product_containers = response.css("li.col-md-3.col-md-3.col-sm-6.col-xs-6 a")
         for product_container in product_containers:
             name = product_container.attrib["title"]
-            url = response.urljoin(product_container.attrib["href"])
+            page_url = response.urljoin(product_container.attrib["href"])
             img_url = product_container.css("img.lazyload").attrib["data-src"]
-            price = product_container.css("div.primary-color.price::text").get()[0:-1]
+            price = product_container.css(f"div.primary-color.price{TEXT_PROP}").get()[
+                :-1
+            ]
             product = {
                 "name": name,
-                "link": url,
+                "product_link": page_url,
                 "img_link": response.urljoin(img_url),
                 "price": price,
             }
@@ -53,13 +57,13 @@ class PakistanStoresSpider(scrapy.Spider):
             yield scrapy.Request(url=next_page_url, callback=self.parse)
         else:
             for product in self.products:
-                link = product["link"]
-                binded_f = partial(self.extract_description, product)
+                link = product["product_link"]
+                extract_product_description = partial(self.extract_description, product)
                 product["description"] = ""
                 if link.__contains__("/product/"):
                     self.increment_and_check()
                 else:
-                    yield scrapy.Request(url=link, callback=binded_f)
+                    yield scrapy.Request(url=link, callback=extract_product_description)
 
     def extract_description(self, product, response):
         """
@@ -72,19 +76,10 @@ class PakistanStoresSpider(scrapy.Spider):
             None
         """
         index = self.products.index(product)
-        description = response.css("div.light p::text")[-1].get()[1:-1]
+        description = response.css(f"div.light p{TEXT_PROP}")[-1].get()[1:-1]
         product["description"] = description
         self.products[index] = product
         self.increment_and_check()
-
-    def write_to_file(self):
-        """
-        This function writes the jsonArray self.products to the file products.json
-        Returns:
-            None
-        """
-        with open("products.json", "w", encoding="UTF-8") as file:
-            json.dump(self.products, file, indent=4)
 
     def increment_and_check(self):
         """
@@ -94,4 +89,4 @@ class PakistanStoresSpider(scrapy.Spider):
         """
         self.processed_products += 1
         if self.processed_products >= len(self.products):
-            self.write_to_file()
+            write_to_file(self.products)
